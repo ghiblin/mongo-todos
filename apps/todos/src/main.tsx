c809f3ad3fcdbd1ba321907a1bb0e53b784e028d
Alessandro Vitali <a.vitali@be-tse.it>
2022-02-25 18:34:10 +0100

    add TodoMVC CSS

--- apps/todos/src/main.tsx
+++ apps/todos/src/main.tsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import App from './app/app';
 import store from './store';
 
+import 'todomvc-app-css/index.css';
+
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
